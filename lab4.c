3c9fe265099f99911dc7cce3e158e43966cfd049
mattcatz <14895427+MattCatz@users.noreply.github.com>
2018-09-10 23:13:14 -0500

    Fixed delay on counter two

--- lab4.c
+++ lab4.c
@@ -8,18 +8,21 @@
 
 #include "STM32L1xx.h" /* Microcontroller information */
 
-#define UP 0;
-#define DOWN 1;
+#define UP 1;
+#define DOWN 0;
+#define TRUE 1;
+#define FALSE 0;
 
 struct {
   unsigned char count;
   unsigned char direction;
+  unsigned char disable;
 } typedef counter;
 
 void delay(void);
 void count(counter* count);
 void setup_pins(void);
-void update_leds(int ping);
+void update_leds(void);
 void setup_interupts(void);
 void counter_factory(counter* c);
 
@@ -29,20 +32,18 @@ counter count_two;
 int main() {
 
   setup_pins();
-  //setup_timer();
   setup_interupts();
   counter_factory(&count_one);
   counter_factory(&count_two);
   GPIOC->BSRR = GPIO_BSRR_BS_8; //Set PC8=1 to turn ON blue LED
-  
+
   __enable_irq();
-  
-  int ping = 0;
+
   while(1) {
-    ping = ~ping;
     count(&count_one);
     count(&count_two);
-    update_leds(ping);
+    update_leds();
+    count_two.disable = ~count_two.disable;
     delay();
   };
 }
@@ -64,19 +65,20 @@ void delay () {
 /* or down.                                          */
 /*---------------------------------------------------*/
 void count(counter* count) {
+  if (count->disable) return;
   if (count->direction) {
-    count->count = (count->count + (10 - 1)) % 10; //DOWN
-  } else {
     count->count = (count->count + 1) % 10; //UP
+  } else {
+    count->count = (count->count + (10 - 1)) % 10; //DOWN
   }
 }
 
 /*---------------------------------------------------*/
 /* Update the LEDS used in the lab                   */
 /*---------------------------------------------------*/
-void update_leds(int ping) {
+void update_leds(void) {
   unsigned short leds = 0;
-  leds = (count_two.count & 0x0F & ping) << 4; // PC[7:4]
+  leds = (count_two.count & 0x0F) << 4; // PC[7:4]
   leds += (count_one.count & 0x0F); // PC[3:0]
   SET_BIT(GPIOC->BSRR, (~leds & 0xFF) << 16); // turn off LEDs
   SET_BIT(GPIOC->BSRR, leds); // turn on LEDs
@@ -102,18 +104,18 @@ void setup_interupts() {
 
   /* Configure GPIO A1 as external inturupt 1 */
   CLEAR_BIT(SYSCFG->EXTICR[0], SYSCFG_EXTICR1_EXTI1);
-  SET_BIT(SYSCFG->EXTICR[0], SYSCFG_EXTICR1_EXTI1_PA);  
+  SET_BIT(SYSCFG->EXTICR[0], SYSCFG_EXTICR1_EXTI1_PA);
 
   /* Unmask EXTI0 and EXTI1 and set both to rising edge trigger*/
   SET_BIT(EXTI->IMR, (EXTI_IMR_MR0 | EXTI_IMR_MR1));
   SET_BIT(EXTI->RTSR, (EXTI_RTSR_TR0 | EXTI_RTSR_TR1));
-	
+
   NVIC_EnableIRQ(EXTI0_IRQn);
   NVIC_EnableIRQ(EXTI1_IRQn);
 
   /* Clear pending bit for EXTI0 and EXTI1 */
-  CLEAR_BIT(EXTI->PR, (EXTI_PR_PR0 | EXTI_PR_PR1));
-	
+  SET_BIT(EXTI->PR, (EXTI_PR_PR0 | EXTI_PR_PR1));
+
   NVIC_ClearPendingIRQ(EXTI0_IRQn);
   NVIC_ClearPendingIRQ(EXTI1_IRQn);
 }
@@ -139,6 +141,7 @@ void EXTI1_IRQHandler() {
 }
 
 void counter_factory(counter* c) {
-	c->count = 0;
-	c->direction = UP;
+  c->count = 0;
+  c->direction = UP;
+  c->disable = FALSE;
 }
